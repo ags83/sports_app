8d062d6883218733562f6bd56632245da044f5c0
Aaron Shaw <a.shaw83@hotmail.com>
2012-10-09 19:41:13 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsApp::Application.config.secret_token = 'fcd825c47c390a554e7186c5affa1a378f62befb5a861bbf93e0c864da9de4418cf92ad5ff316d2f4da5167e40271aeaa70b4a1f6565f5891c02b3cd3bfe7f3a'
