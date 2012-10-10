90a0aefb0b3e6db239627846095d3d72b3fce484
Aaron Shaw <a.shaw83@hotmail.com>
2012-10-10 15:29:27 +0700

    this

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
 	def start
 		session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, REDIRECT_URI)
-		@auth_url =  session[:oauth].url_for_oauth_code() 	
+		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"email") 	
 		puts session.to_s + "<<< session"
 		
   	 redirect_to session[:oauth].url_for_oauth_code()
@@ -14,7 +14,7 @@ def callback
   if params[:code]
   		# acknowledge code and get access token from FB
 		  session[:access_token] = session[:oauth].get_access_token(params[:code])
-	end		
+		end		
 		 # auth established, now do a graph call:
 		@api = Koala::Facebook::API.new(session[:access_token])
 		begin
@@ -22,9 +22,9 @@ def callback
 		rescue Exception=>ex
 			puts ex.message
 		end
-		puts "THHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
-		#@email = @graph_data['email']
-		#@id = @graph_data['id']
+		#puts << "THHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+		@email = @graph_data['email']
+		@id = @graph_data['id']
 		@name = @graph_data['name']
 		
  		respond_to do |format|
