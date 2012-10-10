4151ae928f4825f9b27f358dcf793d290022063c
Aaron Shaw <a.shaw83@hotmail.com>
2012-10-10 07:57:29 +0700

    up

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+
+class PagesController < ApplicationController
+require "koala"
+def start
+  @oauth= Koala::Facebook::OAuth.new(APP_ID, APP_SECRET,REDIRECT_URI)
+  redirect_to @oauth.url_for_oauth_code(:permissions=>"my permissions")
+end
+
+def callback
+  session[:access_token] = @oauth.get_access_token(params[:code])
+  redirect_to(:action=>"my action")
+end
+
+def home
+  @api = Koala::Facebook::API.new(session[:access_token])
+  begin
+    @graph_data = @api.get_object("/me/photos")
+  rescue Exception=>ex
+    puts ex.message
+  end
+
+  @graph_data do |photo|
+    puts "my photo: #{photo.name}" 
+  end
+end
