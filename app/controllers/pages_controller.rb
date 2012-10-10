55f8c7fccc6a04a67e2ce54a529c835c89be6113
Aaron Shaw <a.shaw83@hotmail.com>
2012-10-10 09:11:47 +0700

    this?

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,18 +2,38 @@ require "koala"
 class PagesController < ApplicationController
 
 	def start
-		puts "IIIIIIIIIIIIIIIIIIIIIIIINNNNNNNNNNNNNNNNNNNNNNNNNNN HHHHHHHHHHHHHHHHHHEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRRRRREEEEEEEEEEEEEEEE "
+		session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, REDIRECT_URI)
+		@auth_url =  session[:oauth].url_for_oauth_code() 	
+		puts session.to_s + "<<< session"
 
-  @oauth= Koala::Facebook::OAuth.new(APP_ID, APP_SECRET,REDIRECT_URI)
-  redirect_to @oauth.url_for_oauth_code()
-end
+  	respond_to do |format|
+			 format.html {  }
+		 end
+
+	end
 
 def callback
-  @sess = @oauth.get_access_token(params[:code])
-  puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOEEEEEEEEEEE "
-  respond_to do |format|
+  if params[:code]
+  		# acknowledge code and get access token from FB
+		  session[:access_token] = session[:oauth].get_access_token(params[:code])
+		end		
+
+		 # auth established, now do a graph call:
+
+		@api = Koala::Facebook::API.new(session[:access_token])
+		begin
+			@graph_data = @api.get_object("me")
+		rescue Exception=>ex
+			puts ex.message
+		end
+
+  
+ 		respond_to do |format|
 		 format.html {   }			 
 		end
+
+
+	end
 end
 		 
 	'''  
