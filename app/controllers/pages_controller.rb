82ff6a67e24679e148d4621360d9bd0386018518
Aaron Shaw <a.shaw83@hotmail.com>
2012-10-10 08:05:59 +0700

    this

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,17 @@
 
 class PagesController < ApplicationController
 require "koala"
-def start
-  @oauth= Koala::Facebook::OAuth.new(APP_ID, APP_SECRET,REDIRECT_URI)
-  redirect_to @oauth.url_for_oauth_code(:permissions=>"my permissions")
-end
+	def start
+	  @oauth= Koala::Facebook::OAuth.new(APP_ID, APP_SECRET,REDIRECT_URI)
+	  redirect_to @oauth.url_for_oauth_code(:permissions=>"my permissions")
+	end
 
-def callback
-  session[:access_token] = @oauth.get_access_token(params[:code])
-  redirect_to(:action=>"my action")
-end
+	def callback
+	  session[:access_token] = @oauth.get_access_token(params[:code])
+	  redirect_to(:action=>"my action")
+	end
 
-def home
-  puts("boo")
+	def home
+	  puts("boo")
+	end
 end
