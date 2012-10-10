1d6920c4c9b52231f84edbfb5f29667551e3f1ac
Aaron Shaw <a.shaw83@hotmail.com>
2012-10-10 10:38:25 +0700

    this

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
 	def start
 		session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, REDIRECT_URI)
-		@auth_url =  session[:oauth].url_for_oauth_code() 	
+		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"email") 	
 		puts session.to_s + "<<< session"
 		
   	 redirect_to session[:oauth].url_for_oauth_code()
@@ -15,6 +15,9 @@ def callback
   		# acknowledge code and get access token from FB
 		  session[:access_token] = session[:oauth].get_access_token(params[:code])
 		end		
+		@id = @graph_data['id']
+		
+		
 
 		 # auth established, now do a graph call:
 
@@ -24,38 +27,14 @@ def callback
 		rescue Exception=>ex
 			puts ex.message
 		end
+		@email = @graph_data['email']
 		@name = @graph_data['name']
  		respond_to do |format|
 		 format.html {   }			 
 		end
 
 	end
-		 
-	'''  
-	end
-
-	def callback
-	  if params[:code]
-  		# acknowledge code and get access token from FB
-		  session[:access_token] = session[:oauth].get_access_token(params[:code])
-		end		
-
-		 # auth established, now do a graph call:
-
-		@api = Koala::Facebook::API.new(session[:access_token])
-		begin
-			@graph_data = @api.get_object("my.Name")
-		rescue Exception=>ex
-			puts ex.message
-		end
- 		respond_to do |format|
-		 format.html {   }			 
-		end
-	end
-
-	def home
-	  puts("boo")
-	end
-	
-	'''
 end
+
+def signup
+	
